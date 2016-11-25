10e6203a972ba656e607b6a2db3507df85aa1bd6
Denis Saidov <denis.saidov@kcl.ac.uk>
2016-11-25 13:00:13 +0000

    create testing httpGET method. Pulling data from API.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,19 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import model.testJSONParsing;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
+
+        testJSONParsing test = new testJSONParsing();
+        try {
+            test.httpGET("http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=jsonP&prefix=Getdata");
+        } catch(Exception exe){
+            //Your error handling code
+        }
+
         launch(args);
     }
 
