88a8eafa88f693279bd9d35051ee432fa5b34fd9
k1502799 <k1502799@kcl.ac.uk>
2016-11-25 18:45:27 +0000

    Displays data for multiple countries.

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import jdk.nashorn.internal.scripts.JO;
 import model.testJSONParsing;
 import model.urlBuilder;
 import org.json.JSONArray;
@@ -17,6 +18,9 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Main2 extends Application {
 
@@ -44,39 +48,65 @@ public class Main2 extends Application {
         //test.printJsonObject(jsonObj);
 
 
+        ArrayList<ArrayList<ArrayList<String>>> galaxy = new  ArrayList<ArrayList<ArrayList<String>>>();
+
+        ArrayList<String> countries = new ArrayList<String>();
+
+        countries.add("br");
+        countries.add("gb");
+
+        for (int w = 0; w < countries.size(); w++) {
 
 
-        urlBuilder Bob = new urlBuilder();
+            urlBuilder Bob = new urlBuilder();
 
-        String a =  Bob.URL("br","SP.ADO.TFRT","2004", "2006");
+            String a = Bob.URL(countries.get(w), "SP.ADO.TFRT", "1990", "2006");
 
-        JSONArray Bonobo = test.httpGET(a);
+            JSONArray Bonobo = test.httpGET(a);
 
-        JSONArray John = (JSONArray)Bonobo.get(1);
-        // gets year by index
-        JSONObject Hani = John.getJSONObject(1);
+            JSONArray John = (JSONArray) Bonobo.get(1);
 
-        System.out.println(Hani);
-        //gets value from given year
-        Double value1 = Double.parseDouble(Hani.getString("value"));
-        String year1 = Hani.getString("date");
+            //System.out.println(John);
 
+            ArrayList<ArrayList<String>> outer = new ArrayList<ArrayList<String>>();
 
-
-        JSONObject countryArray = Hani.getJSONObject("country");
-        String countyName = countryArray.getString("value");
-
-       // String coutryName = countryObject.getString("value");
-
-
-
-
-
+            for (int i = John.length() - 1; i >= 0; i--) {
+                ArrayList<String> inner = new ArrayList<String>();
+                JSONObject Jake = John.getJSONObject(i);
+                Double value1 = Double.parseDouble(Jake.getString("value"));
+                String valueX1 = Jake.getString("value");
+                String year1 = Jake.getString("date");
+                JSONObject countryArray = Jake.getJSONObject("country");
+                String countyName = countryArray.getString("value");
+                inner.add(valueX1);
+                inner.add(year1);
+                inner.add(countyName);
+                outer.add(inner);
 
 
+            }
+            System.out.println("outer");
+            System.out.println(outer);
+
+            JSONObject Hani = John.getJSONObject(1);
 
 
-        System.out.println(value1);
+            System.out.println(Hani);
+
+            Double value1 = Double.parseDouble(Hani.getString("value"));
+
+            String year1 = Hani.getString("date");
+            JSONObject countryArray = Hani.getJSONObject("country");
+            String countyName = countryArray.getString("value");
+
+            // String coutryName = countryObject.getString("value");
+
+
+            System.out.println(value1);
+
+            galaxy.add(outer);
+
+        }
         stage.setTitle("Line Chart Sample");
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
@@ -87,12 +117,22 @@ public class Main2 extends Application {
         lineChart.setTitle("Stock Monitoring, 2010");
 
         XYChart.Series series1 = new XYChart.Series();
-        series1.setName(countyName);
+        series1.setName(galaxy.get(0).get(0).get(2));
 
-        series1.getData().add(new XYChart.Data(year1, value1));
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName(galaxy.get(1).get(0).get(2));
+
+
+        for (int i = 0; i < galaxy.get(0).size() ; i++) {
+            series1.getData().add(new XYChart.Data(galaxy.get(0).get(i).get(1), Double.parseDouble(galaxy.get(0).get(i).get(0))));
+        }
+
+        for (int i = 0; i < galaxy.get(1).size() ; i++) {
+            series2.getData().add(new XYChart.Data(galaxy.get(1).get(i).get(1), Double.parseDouble(galaxy.get(1).get(i).get(0))));
+        }
 
         Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().addAll(series1);
+        lineChart.getData().addAll(series1,series2);
 
         stage.setScene(scene);
         stage.show();
