3935e0a2e3a20fd2006cc14c209dc6e2a6c9e313
k1502799 <k1502799@kcl.ac.uk>
2016-11-29 17:25:11 +0000

    comment code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,25 +26,22 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
 
-        String csvFile = "C:\\Users\\Sarosi\\Desktop\\Core\\src\\main\\resources\\storage\\CountryCodesCore.csv";
+        String csvFile = "src\\main\\resources\\storage\\CountryCodesCore.csv";
         CountryReader charles = new CountryReader(csvFile);
         ArrayList<String> lands = new ArrayList<String>();
         lands.add("Hungary");
         lands.add("Bulgaria");
         lands.add("Jordan");
         lands.add("Morocco");
-        CountryNamesToCodes converter = new CountryNamesToCodes();
-        ArrayList<String> counties =  converter.convert(lands,charles);
+        ArrayList<String> counties =  new CountryNamesToCodes().convert(lands,charles);
+        LineChart<String,Number> chart = new ChartBuillder().buildLineChart(new ArrayBuilder().buildArray(counties,"1995","2005","NY.GDP.MKTP.CD"));
 
-
-        LineChart<String,Number> chart = new ChartBuillder().buildLineChart(new ArrayBuilder().buildArray(counties,"1995","2005","NY.GNP.MKTP.CD"));
-
-
-
+        //ADD ERROR HANDLING TO Arraybuider and ChartBuider for when there is no data for given year
+        //ADD CSV of indicators and indicator codes eg.: Gross Domestic Product, NY.GDP.MKTP.CD
 
         primaryStage.setWidth(1080);
         primaryStage.setHeight(640);
-        primaryStage.setTitle("Macro Economics");
+        primaryStage.setTitle("Project Core");
         primaryStage.setScene(new InterfaceScene(primaryStage,chart));
         primaryStage.show();
     }
--- src/main/java/model/CountryNamesToCodes.java
+++ src/main/java/model/CountryNamesToCodes.java
@@ -6,8 +6,15 @@ import java.util.ArrayList;
  * Created by Sarosi on 27/11/2016.
  */
 public class CountryNamesToCodes {
+
     public CountryNamesToCodes(){}
 
+    /**
+     *
+     * @param names the full string name of the countries we are requesting the codes for
+     * @param reader a countryreader which has processed a CSV
+     * @return an ArrayList of country codes
+     */
     public ArrayList<String> convert(ArrayList<String> names, CountryReader reader){
         ArrayList<String> codes = new ArrayList<String>();
         for (int i = 0; i < names.size(); i++) {
